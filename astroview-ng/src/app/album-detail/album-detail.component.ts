9bcdd0aef530cf1e0d9e140d70a444092fcf7c94
Robert Martens <robert.martens@snowsoftware.com>
2021-10-20 20:37:06 -0400

    filtering

--- astroview-ng/src/app/album-detail/album-detail.component.ts
+++ astroview-ng/src/app/album-detail/album-detail.component.ts
@@ -25,6 +25,9 @@ export class AlbumDetailComponent implements OnInit {
   stats = new AlbumStats();
   images: ImageDetail[] = [];
 
+  filter_exposure?: string | number;
+  filter_iso?: number;
+
   getAlbumDetail(): void {
     const unique_name = this.route.snapshot.paramMap.get('unique_name')!;
     this.albumService.getAlbumDetail(unique_name)
@@ -33,8 +36,28 @@ export class AlbumDetailComponent implements OnInit {
     this.albumService.getAlbumStats(unique_name)
       .subscribe(stats => this.stats = stats);
 
-    this.albumService.getImages(unique_name)
-      .subscribe(images => this.images = images);
+    this.getImages(unique_name);
+
   }
+
+  getImages(unique_name: string): void {
+    this.albumService.getImages(unique_name)
+      .subscribe(images => this.images = images.filter(img => 
+        ((this.filter_iso ?? img.iso) == img.iso) && ((this.filter_exposure ?? img.exposure) == img.exposure)
+      ));
+
+  }
+
+  setFilter(iso?: string, exposure?: string): void {
+    if (iso) {
+      this.filter_iso = parseInt(iso);
+    } else {
+      this.filter_iso = undefined;
+    }
+    this.filter_exposure = exposure;
+
+    this.getImages(this.album.unique_name);
+  }
+
 
 }
--- astroview-ng/src/app/album-stats.ts
+++ astroview-ng/src/app/album-stats.ts
@@ -1,4 +1,6 @@
+type Exposure = string | number;
+
 export class AlbumStats {
-    exposures: (string|number)[] = [];
-    exp_by_iso: object = {};
+    exposures: string[] = [];
+    exp_by_iso: {[iso: number]: {[exp: string]: number}} = {};
 }
